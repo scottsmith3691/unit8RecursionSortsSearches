b35b11677355d77614c89ded4630a5efe3244a52
scottsmith3691 <thesmithinator3691@gmail.com>
2016-03-25 17:02:40 -0500

    3/25/16

--- FractalTreeLab/KochPanel.java
+++ FractalTreeLab/KochPanel.java
@@ -1,55 +1,58 @@
-//********************************************************************
-//  KochPanel.java       Author: Lewis/Loftus/Cocking
-//
-//  Represents a drawing surface on which to paint a Koch Snowflake.
-//********************************************************************
-
 import java.awt.*;
 import java.util.*;
 import javax.swing.JPanel;
 import java.awt.geom.*;
 public class KochPanel extends JPanel
 {
+   //Initializes the width of the panel
    private final int PANEL_WIDTH = 800;
+   
+   //Initializes the height of the panel
    private final int PANEL_HEIGHT = 800;
+   
+   //Initializes the array list of colors
    private ArrayList<Color> color = new ArrayList<Color>();
    
+   //Initializes the random generator
    private Random rn = new Random();
-   //-----------------------------------------------------------------
-   //  Sets the initial fractal order to the value specified.
-   //-----------------------------------------------------------------
+   
+   //Constructor for Koch Panel, sets background and dimensions
    public KochPanel ()
    {
       setBackground (Color.BLACK);
       setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
    }
 
-   //-----------------------------------------------------------------
-   //  Draws the fractal recursively. Base case is an order of 1 for
-   //  which a simple straight line is drawn. Otherwise three
-   //  intermediate points are computed, and each line segment is
-   //  drawn as a fractal.
-   //-----------------------------------------------------------------
+   //  Draws the fractal recursively
     
    public void drawFractal (double dLength, int startX, int startY, double angle,
                             Graphics2D g2, int count)
    {    
+        //Initializes all varabiable neccesary for drawing
         int endX, endY, endX1, endY1;
+        
+        //Creates the random generator of index's 0-7
         int rand = rn.nextInt(8);
+        
+        //Sets the color randomly
         g2.setColor(color.get(rand));
+        
+        //End case
         if(count==15)
         {
             return;
         }
        
-        
+        //calculates the end integers 
         endX = startX + (int) (Math.cos(Math.toRadians(angle)) * dLength );
-       endY = startY + (int) (Math.sin(Math.toRadians(angle)) * dLength );
-      
-      g2.drawLine(startX, startY, endX, endY);
-      
-      drawFractal(dLength*0.80, endX, endY, angle + 50, g2, count+1);
-      drawFractal(dLength*0.60, endX, endY, angle - 35, g2, count+1);
+        endY = startY + (int) (Math.sin(Math.toRadians(angle)) * dLength );
+        
+        //draws the line(s)
+        g2.drawLine(startX, startY, endX, endY);
+        
+        //calls itself to draw two lines off the original line
+        drawFractal(dLength*0.80, endX, endY, angle + 50, g2, count+1);
+        drawFractal(dLength*0.60, endX, endY, angle - 35, g2, count+1);
    }
 
    //-----------------------------------------------------------------
@@ -57,9 +60,11 @@ public class KochPanel extends JPanel
    //-----------------------------------------------------------------
    public void paintComponent(Graphics g)
    {
-      super.paintComponent(g);
-      Graphics2D g2 = (Graphics2D) g;
-      
+        //calls its parent class
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        
+        //adds every color to the color array list
         color.add(Color.RED);
         color.add(Color.CYAN);
         color.add(Color.BLUE);
@@ -69,8 +74,9 @@ public class KochPanel extends JPanel
         color.add(Color.ORANGE);
         color.add(Color.PINK);
         color.add(Color.WHITE);
-      
-      drawFractal (100, 300, 250, 45, g2, 1);
+        
+        //Initial call to the fractal method
+        drawFractal (100, 300, 250, 45, g2, 1);
       
    }
 
